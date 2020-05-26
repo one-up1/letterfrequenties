f339afc4134bdfbf80e0fbb9760bd5561952a918
0ne_Up <oneup1@gmail.com>
2020-05-26 11:45:54 +0200

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Letterfrequenties
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.button = new System.Windows.Forms.Button();
+            this.cbCaseSensitive = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox.Location = new System.Drawing.Point(12, 12);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox.Size = new System.Drawing.Size(560, 185);
+            this.textBox.TabIndex = 0;
+            // 
+            // button
+            // 
+            this.button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button.Location = new System.Drawing.Point(12, 226);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(75, 23);
+            this.button.TabIndex = 1;
+            this.button.Text = "Letters tellen";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // cbCaseSensitive
+            // 
+            this.cbCaseSensitive.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.cbCaseSensitive.AutoSize = true;
+            this.cbCaseSensitive.Location = new System.Drawing.Point(12, 203);
+            this.cbCaseSensitive.Name = "cbCaseSensitive";
+            this.cbCaseSensitive.Size = new System.Drawing.Size(118, 17);
+            this.cbCaseSensitive.TabIndex = 2;
+            this.cbCaseSensitive.Text = "Hoofdlettergevoelig";
+            this.cbCaseSensitive.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 261);
+            this.Controls.Add(this.cbCaseSensitive);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.textBox);
+            this.Name = "MainForm";
+            this.Text = "Letterfrequenties";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.CheckBox cbCaseSensitive;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Letterfrequenties
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            // Type parameters moeten toch classes zijn? (<Character, Integer>) ?
+            Dictionary<char, int> letterCounts = new Dictionary<char, int>();
+            int total = 0, ignored = 0;
+
+            foreach (char c in textBox.Text)
+            {
+                // In de opdracht staat "letters" dus andere tekens gaan we negeren.
+                if (!Char.IsLetter(c))
+                {
+                    ignored++;
+                    continue;
+                }
+                total++;
+
+                // Als "case sensitive" niet is aangevinkt
+                // doen we alsof alle letters kleine letters zijn.
+                char letter = cbCaseSensitive.Checked ? c : Char.ToLower(c);
+
+                // C# doet alsof Dictionary een primitieve array is?
+                letterCounts[letter] = letterCounts.ContainsKey(letter) ?
+                    letterCounts[letter] + 1 : 1;
+            }
+
+            if (total == 0)
+            {
+                MessageBox.Show("Geen letters gevonden!");
+                return;
+            }
+
+            // Wel leuk om de letters te sorteren.
+            List<Char> letters = letterCounts.Keys.ToList();
+            letters.Sort();
+
+            StringBuilder sb = new StringBuilder(letters.Count +
+                " verschillende letters gevonden in " + textBox.TextLength + " tekens.");
+            sb.AppendLine();
+            sb.Append(total + " letters in totaal, " + ignored + " tekens genegeerd.");
+            sb.AppendLine();
+            foreach (char letter in letters)
+            {
+                sb.AppendLine();
+                sb.Append(letter + ": " + letterCounts[letter]);
+            }
+            MessageBox.Show(sb.ToString());
+        }
+    }
+}
